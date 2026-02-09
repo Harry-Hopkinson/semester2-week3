1b1cb8383cff717c05d26cab62308a297da4c459
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2026-02-09 12:53:01 +0000

    Start working on Salary portfolio task

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,45 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Harry Hopkinson
+ * ID: 201956529
  */
 
- int main( void ) {
+ int main( void )
+ {
+    // define and initialise variables for the problem data
+    int salary = 40000;
+    float nationalInsurance = 5.00, taxRate = 15.00;
 
-    // define and initialise variables for the problem data 
+    if (salary < 14000 || salary > 40000)
+    {
+        printf("Salary can only be in the range £14,000 and £40,000.\n");
+        return 1;
+    }
+    else if (nationalInsurance < 0 || nationalInsurance > 10)
+    {
+        printf("National insurance can only be in the range 0-10 percent.\n");
+        return 1;
+    }
+    else if (taxRate < 10 || taxRate > 30)
+    {
+        printf("Tax rate can only be in the range 10-30 percent.\n");
+        return 1;
+    }
 
     // calculate the deductions and final take-home salary
+    float niContribution = salary * (nationalInsurance / 100);
+    float taxContribution = 0;
+    if (salary - niContribution > 12500)
+    {
+        float taxContribution = ((salary - niContribution) - 12500) * (taxRate / 100);
+    }
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £%d\n", salary);
+    printf("NI contribution £%.2f\n", niContribution);
+    printf("Tax contribution £%.2f\n", taxContribution);
+    // printf("Take home salary £%d",var_name);
 
     return 0;
  }
